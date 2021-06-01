ebfeaed4dd00c54028d020a6449841e9c5a9fefa
arppy <berta@inf.u-szeged.hu>
2021-06-01 18:39:51 +0200

    memory limit

--- choose_gpu_tf2.py
+++ choose_gpu_tf2.py
@@ -5,4 +5,5 @@ gpus = tf.config.list_physical_devices('GPU')
 selected = gpus[0]
 tf.config.set_visible_devices(selected, 'GPU')
 tf.config.experimental.set_memory_growth(selected, True)
-tf.config.experimental.set_virtual_device_configuration( selected, [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=FLAGS.memory_limit)])
+memory_limit = 10000
+tf.config.experimental.set_virtual_device_configuration( selected, [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory_limit)])
