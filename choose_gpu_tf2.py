7bb63ee46908edc25d7645608dadbc19b3b31a09
arppy <berta@inf.u-szeged.hu>
2021-06-01 18:37:47 +0200

    scripts for choose gpu in tf2

---
+++ choose_gpu_tf2.py
@@ -0,0 +1,8 @@
+import os
+import tensorflow as tf
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+gpus = tf.config.list_physical_devices('GPU')
+selected = gpus[0]
+tf.config.set_visible_devices(selected, 'GPU')
+tf.config.experimental.set_memory_growth(selected, True)
+tf.config.experimental.set_virtual_device_configuration( selected, [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=FLAGS.memory_limit)])
