8ae3965aa92d3911fe057cc16153b634bb8d1b15
arppy <berta@inf.u-szeged.hu>
2021-11-05 19:21:21 +0100

    tiny imagenet preprocess

---
+++ tiny_imagenet_val_class_mkdir.py
@@ -0,0 +1,29 @@
+import os
+
+DATA_PATH = '../res/data/'
+TINY_IMAGENET_VALID = DATA_PATH+'tiny-imagenet-200/valid'
+
+# Create separate validation subfolders for the validation images based on
+# their labels indicated in the val_annotations txt file
+val_img_dir = os.path.join(TINY_IMAGENET_VALID, 'images')
+
+# Open and read val annotations text file
+fp = open(os.path.join(TINY_IMAGENET_VALID, 'val_annotations.txt'), 'r')
+data = fp.readlines()
+
+# Create dictionary to store img filename (word 0) and corresponding
+# label (word 1) for every line in the txt file (as key value pair)
+val_img_dict = {}
+for line in data:
+    words = line.split('\t')
+    val_img_dict[words[0]] = words[1]
+fp.close()
+
+# Create subfolders (if not present) for validation images based on label,
+# and move images into the respective folders
+for img, folder in val_img_dict.items():
+    newpath = (os.path.join(val_img_dir, folder))
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    if os.path.exists(os.path.join(val_img_dir, img)):
+        os.rename(os.path.join(val_img_dir, img), os.path.join(newpath, img))
