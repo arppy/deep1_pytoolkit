7c3cdd587fd1bca9d3a56dc3c0fa100f764daf9a
arppy <berta@inf.u-szeged.hu>
2024-04-25 13:44:53 +0200

    create lab file

--- commonvoices_to_mfa.py
+++ commonvoices_to_mfa.py
@@ -1,26 +1,26 @@
 import re
 import os
 
-corpus_dir="/home/berta/data/cv-corpus-17.0-2024-03-15/hu/"
+corpus_folder= "/home/berta/data/cv-corpus-17.0-2024-03-15/hu/"
 clips_folder="clips_by_user_with_text/"
 try:
-   os.makedirs(corpus_dir+clips_folder)
+   os.makedirs(corpus_folder + clips_folder)
 except FileExistsError:
    pass
 description_file_name = "validated.tsv"
 
-with open(corpus_dir+description_file_name) as description_file:
+with open(corpus_folder + description_file_name) as description_file:
    next(description_file)
    for line in description_file:
       split_line = re.split(r'\t+', line)
-      print(split_line[0], split_line[1], split_line[3])
+      client_folder= split_line[0][:25] + "/"
       try:
-         os.makedirs(corpus_dir+clips_folder+split_line[0])
+         os.makedirs(corpus_folder + clips_folder + client_folder)
       except FileExistsError:
          pass
       clip_name = split_line[1]
       base_clip_name = clip_name.rsplit('.', 1)[0]
-      print(corpus_dir+clips_folder+split_line[0]+"/"+base_clip_name+".lab")
-      #with open(corpus_dir+clips_folder+split_line[0]+"/"+, 'a') as f:
-      #   print('hello world', file=f)
+      lab_file_name = corpus_folder + clips_folder + client_folder + base_clip_name + ".lab"
+      with open(lab_file_name, 'w') as lab_file:
+         print(split_line[3], file=lab_file)
       break
