c364a87da8edaaf8604f3d5b689e6b0f878a62cc
arppy <berta@inf.u-szeged.hu>
2024-04-25 14:04:30 +0200

    copy

--- commonvoices_to_mfa.py
+++ commonvoices_to_mfa.py
@@ -2,6 +2,7 @@ import re
 import os
 
 corpus_folder= "/home/berta/data/cv-corpus-17.0-2024-03-15/hu/"
+original_clips_folder="clips/"
 clips_folder="clips_by_user_with_text/"
 try:
    os.makedirs(corpus_folder + clips_folder)
@@ -20,7 +21,7 @@ with open(corpus_folder + description_file_name) as description_file:
          pass
       clip_name = split_line[1]
       base_clip_name = clip_name.rsplit('.', 1)[0]
+      os.system('cp '+corpus_folder+original_clips_folder+clip_name+' '+corpus_folder+clips_folder+client_folder+clip_name)
       lab_file_name = corpus_folder + clips_folder + client_folder + base_clip_name + ".lab"
       with open(lab_file_name, 'w') as lab_file:
          print(split_line[3], file=lab_file)
-      break
